5974e845f0b91a444620eadf202ccff112181563
Jiangxiaowei <1257101304@qq.com>
2022-06-10 10:41:45 +0800

    基本配置

---
+++ learing_Thread/src/main/java/com/xwnb/LearingThreadApplication.java
@@ -0,0 +1,13 @@
+package com.xwnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearingThreadApplication.class, args);
+    }
+
+}
---
+++ learing_Thread/src/main/java/com/xwnb/Spring自带线程池ThreadPoolTaskExecutor.java
@@ -0,0 +1,7 @@
+package com.xwnb;
+
+/**
+ * @author SB_AXN
+ */
+public class Spring自带线程池ThreadPoolTaskExecutor {
+}
